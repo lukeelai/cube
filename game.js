64315863e0dd29debad4c85041e04881b6a7bad2
Luke Lai <uklelai@gmail.com>
2016-09-15 18:33:06 -0700

    Create game.js

---
+++ game.js
@@ -0,0 +1,17 @@
+var canvas = document.getElementById("myCanvas");
+var cxt= canvas.getContext("2d");
+var myCube;
+var x = 50;
+var y = 550;
+
+cxt.rect(x,y,50,50);
+cxt.stroke();
+
+document.body.onkeyup = function(e){
+    if(e.keyCode == 32){
+    	x+=100;
+        y-=100;
+        cxt.rect(x,y,50,50);
+		cxt.stroke();
+    }
+} 
