e29f2b3fce0dbc82190cd6c66db0373eea1ac5f7
Luke Lai <uklelai@gmail.com>
2016-09-16 21:41:27 -0700

    Update game.js

--- game.js
+++ game.js
@@ -1,17 +1,32 @@
 var canvas = document.getElementById("myCanvas");
-var cxt= canvas.getContext("2d");
-var myCube;
+var ctx = canvas.getContext("2d");
+var radius = 50;
 var x = 50;
-var y = 550;
+var y = 5;
+var dx = 2;
+var dy = -2;
 
-cxt.rect(x,y,50,50);
-cxt.stroke();
+function drawRect() {
+    ctx.beginPath();
+    ctx.rect(x, y, radius, radius);
+    ctx.fillStyle = "red";
+    ctx.fill();
+    ctx.closePath();
+}
 
-document.body.onkeyup = function(e){
-    if(e.keyCode == 32){
-    	x += 100;
-        y -= 100;
-        cxt.rect(x,y,50,50);
-		cxt.stroke();
-    } 
-} 
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawRect();
+    
+    if(x + dx > canvas.width-radius || x + 50 < radius) {
+        dx = -dx;
+    }
+    if(y + dy > canvas.height-radius || y + 50 < radius) {
+        dy = -dy;
+    }
+    
+    x += dx;
+    y += dy;
+}
+
+setInterval(draw, 10);
